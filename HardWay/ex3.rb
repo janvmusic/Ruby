196fbfb4b635b6dfac8a92676203f7a2e470b360
Jorge Nunez <djmatt25@gmail.com>
2013-01-21 23:00:58 -0700

    Ex3

---
+++ HardWay/ex3.rb
@@ -0,0 +1,29 @@
+puts "I will now count my chickens"
+
+# Divide 30 / 6 which is 5 and then adds 25 that's why its 30
+puts "Hens", 25 + 30 / 6
+
+# Mutiply 25 * 3 which is 75, then 75 mod 4 is 3, 100 - 3 is 97
+puts "Roosters", 100 - 25 * 3 % 4
+
+puts "Now I'll count the eggs"
+# Divide 1/4 then gets the mod 4, after that adds the other number, 
+#since its an int object it will get rounded to 7, the real value is
+#6.75
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+#adds 3 + 2 then compare with the 5 -7
+puts 3 + 2 < 5 - 7
+
+#adds 3 + 2
+puts "What is 3 + 5", 3 + 2
+#adds 5 - 7
+puts "What is 5 - 7", 5 - 7
+
+puts "That's why it's false."
+puts "How about some more"
+
+puts "Is it greater?", 5 > -2
+puts "Is it greater or equal?", 5 >= -2
+puts "Is it less or equal?", 5 <= -2
