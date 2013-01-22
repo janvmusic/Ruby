33b56b876bb332303dc469c68df3d885811c2a0b
Jorge Nunez <djmatt25@gmail.com>
2013-01-21 22:41:12 -0700

    The Hard Way
    Ex1 & Ex2 + Doc

---
+++ HardWay/ex1.rb
@@ -0,0 +1,6 @@
+puts "Hello World"
+#puts "Hello Again"
+#puts "I like typing like this."
+#puts "This ain't fun ._."
+puts "Hey I'm printing!!!"
+puts "Don't touch my mazapan D:!"
---
+++ HardWay/ex2.rb
@@ -0,0 +1,9 @@
+#A comment, this is so you can read your program later
+#Anything after the # is ignored by ruby
+
+puts "I could have coded like this." #Omg again a comment XD
+
+#You can also use a comment to disable or comment out a piece of code:
+#puts "This won't appear"
+
+puts "This will appear"
