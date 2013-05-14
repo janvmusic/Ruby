618fb4ab1a0f67cfb07d9a4263f4de228ea8032f
Jorge Nunez <djmatt25@gmail.com>
2013-05-14 07:02:40 -0600

    Work of ex50

---
+++ HardWay/projects/ex51/lib/ex51.rb
@@ -0,0 +1,17 @@
+require_relative "ex51/version"
+require "sinatra"
+require "erb"
+
+get '/' do
+    greeting = "Hello, World!"
+    erb :index, :locals => {:greeting => greeting}
+end
+
+get '/hello' do 
+    erb :hello_form
+end
+
+post '/hello' do
+    greeting = "#{params[:greet] || "Hello"}, #{params[:name] || "Nobody"}" 
+    erb :index, :locals => {:greeting => greeting}
+end
