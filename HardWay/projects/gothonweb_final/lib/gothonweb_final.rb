ff0865d99616db75d0dbee17d9875ba993623432
Jorge Nunez <djmatt25@gmail.com>
2013-05-19 12:39:19 -0600

    Added Final exercise
    Working on make it work!

--- HardWay/projects/gothonweb_final/lib/gothonweb_final.rb
+++ HardWay/projects/gothonweb_final/lib/gothonweb_final.rb
@@ -1,17 +1,31 @@
 require_relative "gothonweb_final/version"
+require_relative "map"
 require "sinatra"
 require "erb"
 
+use Rack::Session::Pool
+
 get '/' do
-    greeting = "Hello, World!"
-    erb :index, :locals => {:greeting => greeting}
+    # This is used to "setup" the session with starting values
+    p START
+    session[:room] = START
+    redirect("/game")
 end
 
-get '/hello' do 
-    erb :hello_form
+get '/game' do 
+    if session[:room]
+        erb :show_room, :locals => {:room => session[:room]}
+    else
+        # Why is this here? do you need it?
+        erb :you_died
+    end 
 end
 
-post '/hello' do
-    greeting = "#{params[:greet] || "Hello"}, #{params[:name] || "Nobody"}" 
-    erb :index, :locals => {:greeting => greeting}
+post '/game' do
+    action = "#{params[:action] || nil}"
+    # There is a bug here, can you fix it?
+    if session[:room]
+        session[:room] = session[:room].go(params[:action])
+    end
+    redirect("/game")
 end
